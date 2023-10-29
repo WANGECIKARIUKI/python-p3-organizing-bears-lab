22f10cd6695fd39abd4b7d634ff6d2c4cdaabb4c
LUCY <wangecikariu.luc@gmail.com>
2023-10-29 13:46:18 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 7, 'M', 'black', 'active', 1),
+                                                                    ('Rowdy', 10, 'M', 'purple', 'active', 1),
+                                                                    ('Tabitha', 3, 'F', 'red', 'jovial', 1),
+                                                                    ('Sergeant Brown', 8, 'M', 'black', 'stern', 1),
+                                                                    ('Melissa', 12, 'F', 'caucasian', 'happy', 1),
+                                                                    ('Grinch', 7, 'M', 'green', 'moody', 1),
+                                                                    ('Wendy', 6, 'F', 'black', 'talkative', 1),
+                                                                    (NULL, 5, 'F', 'yellow', 'introvert', 1);
